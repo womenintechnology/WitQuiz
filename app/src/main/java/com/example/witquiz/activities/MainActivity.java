8f22b50f22ede84421fc5e90f1a0ec35f93f3958
Aleksandra Majchrzak <briza.6@gmail.com>
2016-04-20 23:00:52 +0200

    Added MenuActivity and connection MainActivity -> MenuActivity with intent.

---
+++ app/src/main/java/com/example/witquiz/activities/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.witquiz.activities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.witquiz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String SHARED_PREFERENCES = "WIT_PREFERENCES";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadControls();
+    }
+
+    private void loadControls() {
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userNameEditText = (EditText) findViewById(R.id.userName_editText);
+                String userName = userNameEditText.getText().toString();
+
+                if(userName.isEmpty()){
+                    Toast.makeText(MainActivity.this, getString(R.string.set_username), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                SharedPreferences shared = getSharedPreferences(SHARED_PREFERENCES, 0);
+                shared.edit()
+                        .putString("CurrentUser", userName)
+                        .apply();
+
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/witquiz/activities/MenuActivity.java
@@ -0,0 +1,19 @@
+package com.example.witquiz.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.witquiz.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+}
