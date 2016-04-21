cd8ed8656507df23e5efea32b5a2df7672ddd5d2
Aleksandra Majchrzak <briza.6@gmail.com>
2016-04-21 22:28:43 +0200

    Added category creator activity.

---
+++ app/src/main/java/com/example/witquiz/activities/CreatorActivity.java
@@ -0,0 +1,155 @@
+package com.example.witquiz.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.witquiz.R;
+import com.example.witquiz.databasemanager.DatabaseManager;
+import com.example.witquiz.entities.Category;
+
+public class CreatorActivity extends AppCompatActivity {
+
+    private RadioButton editCategoryRadio;
+    private RadioButton newCategoryRadio;
+
+    private Spinner categorySpinner;
+    private EditText newCategoryEditText;
+    private Button nextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creator);
+
+        loadControls();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        Category[] categories = DatabaseManager.getAllCategories(false);
+
+        categorySpinner.setAdapter(new ArrayAdapter<Category>(this, R.layout.simple_row, categories){
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+                if(convertView == null){
+                    convertView = CreatorActivity.this.getLayoutInflater()
+                            .inflate(R.layout.simple_row, null);
+                }
+
+                TextView categoryTextView = (TextView) convertView.findViewById(R.id.simple_row_textView);
+
+                categoryTextView.setText(this.getItem(position).getName());
+
+                return convertView;
+            }
+
+            @Override
+            public View getDropDownView (int position, View convertView, ViewGroup parent){
+
+                if(convertView == null){
+                    convertView = CreatorActivity.this.getLayoutInflater()
+                            .inflate(R.layout.simple_row, null);
+                }
+
+                TextView categoryTextView = (TextView) convertView.findViewById(R.id.simple_row_textView);
+
+                categoryTextView.setText(this.getItem(position).getName());
+
+                return convertView;
+
+            }
+
+        });
+    }
+
+    private void loadControls() {
+        editCategoryRadio = (RadioButton) findViewById(R.id.edit_category_radioButton);
+        newCategoryRadio = (RadioButton) findViewById(R.id.new_category_radioButton);
+
+        categorySpinner = (Spinner) findViewById(R.id.category_spinner);
+        newCategoryEditText = (EditText) findViewById(R.id.new_category_editText);
+        nextButton = (Button) findViewById(R.id.creator_next_button);
+
+        categorySpinner.setEnabled(editCategoryRadio.isChecked());
+        newCategoryEditText.setEnabled(newCategoryRadio.isChecked());
+
+
+        editCategoryRadio.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                categorySpinner.setEnabled(isChecked);
+            }
+        });
+
+        newCategoryRadio.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                newCategoryEditText.setEnabled(isChecked);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Category selectedCategory;
+
+                if(categorySpinner.isEnabled()){
+
+                    if(categorySpinner.getCount() == 0){
+                        Toast.makeText(CreatorActivity.this, R.string.no_categories_to_edit, Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    selectedCategory = (Category) categorySpinner.getSelectedItem();
+                }
+                else{
+
+                    String newCategoryName = newCategoryEditText.getText().toString();
+
+                    for(int i =0; i< categorySpinner.getCount(); ++i){
+
+                        Category cat = (Category) categorySpinner.getItemAtPosition(i);
+
+                        if(cat.getName().equals(newCategoryName)){
+                            Toast.makeText(CreatorActivity.this, R.string.category_name_exists, Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                    }
+
+                    if(newCategoryName.isEmpty()){
+                        Toast.makeText(CreatorActivity.this, R.string.category_name_cannot_be_empty, Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    selectedCategory = DatabaseManager.createNewCategory(newCategoryName);
+                }
+
+            //    start new activity here
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/witquiz/activities/MenuActivity.java
+++ app/src/main/java/com/example/witquiz/activities/MenuActivity.java
@@ -1,19 +1,40 @@
 package com.example.witquiz.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.witquiz.R;
 
 public class MenuActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        loadControls();
     }
+
+    private void loadControls() {
+
+        Button newGameButton = (Button) findViewById(R.id.new_game_button);
+        Button highScoresButton = (Button) findViewById(R.id.best_scores_button);
+        Button creatorButton = (Button) findViewById(R.id.creator_button);
+        Button exitButton = (Button) findViewById(R.id.exit_button);
+
+        creatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, CreatorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
 }
